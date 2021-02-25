a728a92116f8a3fa0cdd9b34291a1f6393d00e62
test-my-site <78685661+test-my-site@users.noreply.github.com>
2021-02-25 22:33:50 +0800

    first version

---
+++ index.js
@@ -0,0 +1,14 @@
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 100)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    // document.querySelector('body').style.opacity = 1
+    // alert('hi')
+    setTimeout(function() { 
+        document.querySelector('body').style.opacity = 1
+    }, 100)
+})
